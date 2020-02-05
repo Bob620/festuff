5a4c45e7ee90434d259d8d0b3defa352a1ccb6db
bob620 <bruder.kraft225@gmail.com>
2020-02-05 16:00:44 -0600

    laptop broke

--- cli.mjs
+++ cli.mjs
@@ -12,10 +12,37 @@
 // Load spec1, spec2 -> Range Subtraction -> Min/Max
 
 import commandJson from './commands.json';
+import constants from './constants.json';
+import optionsJson from './options.json';
+
 import {ArrayIter} from "./util/interator.mjs";
+
+let env = {};
 
 const args = process.argv.slice(2);
 
+args.reduce(([commands, last], item) => {
+	let data = undefined;
 
+	if (last) {
+
+	} else if (item.startsWith('-')) {
+		const name = item.slice(1);
+		const type = commandJson[name];
+
+		if (type === undefined)
+			throw `Unknown command '${name}'`;
+
+		data = {
+			name,
+			type,
+			validArgs: type.arguments.map(e => e.name),
+			args: {}
+		};
+	} else
+		throw `Unknown argument '${item}'`;
+
+	return [commands, data];
+}, [[], undefined]);
 
 console.log();
