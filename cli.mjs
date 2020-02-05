95f0ad9e6f42353e2fb447c8606b840c98073597
Bob620 <bruder.kraft225@gmail.com>
2020-02-05 14:23:43 -0600

    feat(commands) initial command structure

---
+++ cli.mjs
@@ -0,0 +1,21 @@
+// Peak Find
+//  Custom Limits
+// Background Interpolation and Subtraction
+// Integration
+// Range Subtraction/Addition
+//   Max/Min/Avg
+// Peak fit
+//   FWHM
+//   Center
+// Ratios
+//
+// Load spec1, spec2 -> Range Subtraction -> Min/Max
+
+import commandJson from './commands.json';
+import {ArrayIter} from "./util/interator.mjs";
+
+const args = process.argv.slice(2);
+
+
+
+console.log();
