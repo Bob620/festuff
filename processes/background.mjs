6b4d6788753cd588162eb9a80a70c4f176b93a3c
bob620 <bruder.kraft225@gmail.com>
2020-09-15 20:18:59 -0500

    fix(linear) handle BasicIter

--- processes/background.mjs
+++ processes/background.mjs
@@ -4,9 +4,12 @@ export function linearBackground(leftIndex = 0, rightIndex = 0, line = [0]) {
 	if (leftIndex < 0 || rightIndex >= line.length)
 		throw "Out of range";
 
+	if (rightIndex === 0)
+		rightIndex = line.length
+
 	const length = rightIndex - leftIndex;
-	const initial = line[leftIndex];
-	const change = (line[rightIndex] - initial) / length;
+	const initial = Array.isArray(line) ? line[leftIndex] : line.get(leftIndex);
+	const change = ((Array.isArray(line) ? line[rightIndex] : line.get(rightIndex)) - initial) / length;
 
 	const changeFunc = (change, initial, current) => initial + (change * current);
 
