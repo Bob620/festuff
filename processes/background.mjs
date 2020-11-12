6ab3cc9f983fa5b91abc7d6eec5fd48679504022
bob620 <bruder.kraft225@gmail.com>
2020-11-12 03:53:19 -0600

    fix(linearBackground) correct change func

--- processes/background.mjs
+++ processes/background.mjs
@@ -1,20 +1,20 @@
 import {BasicIter} from '../util/interator.mjs';
 
-export function linearBackground(leftIndex = 0, rightIndex = 0, line = [0]) {
+export function linearBackground(line = [0], leftIndex = 0, rightIndex = 0) {
 	if (leftIndex < 0 || rightIndex >= line.length)
 		throw "Out of range";
 
 	if (rightIndex === 0)
-		rightIndex = line.length
+		rightIndex = line.length - 1
 
-	const length = rightIndex - leftIndex;
+	const length = rightIndex - leftIndex + 1;
 	const initial = Array.isArray(line) ? line[leftIndex] : line.get(leftIndex);
 	const change = ((Array.isArray(line) ? line[rightIndex] : line.get(rightIndex)) - initial) / length;
 
-	const changeFunc = (change, initial, current) => initial + (change * current);
+	const changeFunc = pos => initial + (change * pos);
 
 	return new BasicIter({
-		changeFunc: changeFunc.bind(undefined, change, initial),
+		changeFunc,
 		length
 	});
 }
