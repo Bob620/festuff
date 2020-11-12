bf772f040fe0c71c3c0a8a03e162c85e629073df
bob620 <bruder.kraft225@gmail.com>
2020-11-12 03:54:35 -0600

    chore(reverse) clean change func

--- processes/reverse.mjs
+++ processes/reverse.mjs
@@ -1,9 +1,10 @@
 import {BasicIter} from "../util/interator.mjs";
 
 export function reverse(line = [0]) {
-	const reverse = (line, pos) => line.get(line.length - (pos + 1));
+	const reverse = pos => line.get(line.length - (pos + 1));
+
 	return new BasicIter({
-		changeFunc: reverse.bind(undefined, line),
+		changeFunc: reverse.bind(undefined),
 		length: line.length
 	})
 }
