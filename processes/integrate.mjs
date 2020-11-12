40aa26d5858f66b4b4806c97902b628ae76f8d85
bob620 <bruder.kraft225@gmail.com>
2020-11-12 03:54:05 -0600

    fix(integrate) remove "duel" integrate

--- processes/integrate.mjs
+++ processes/integrate.mjs
@@ -1,36 +1,19 @@
 import {BasicIter} from "../util/interator.mjs";
 
 export function basicIntegration(line = [0]) {
-	const integrate = (line, pos) => {
-		if (pos > 0)
-			return line.get(pos) - line.get(pos - 1);
-		return 0;
+	const integrate = pos => {
+		if (pos === 0)
+			return 0;
+
+		return line.get(pos) - line.get(pos - 1);
 	};
 
 	return new BasicIter({
-		changeFunc: integrate.bind(undefined, line),
+		changeFunc: integrate,
 		length: line.length
-	});
-}
-
-export function duelIntegration(lineOne = [0], lineTwo = [0]) {
-	const integrate = (lineOne, lineTwo, pos) => {
-		if (pos > 0) {
-			const one = lineOne.length > pos ? lineOne.get(pos) : 0;
-			const two = lineTwo.length > pos ? lineTwo.get(pos) : 0;
-
-			return one - two;
-		}
-		return 0;
-	};
-
-	return new BasicIter({
-		changeFunc: integrate.bind(undefined, lineOne, lineTwo),
-		length: lineOne.length < lineTwo.length ? lineTwo.length : lineOne.length
 	});
 }
 
 export default {
 	basicIntegration,
-	duelIntegration
 };
